2cb0edfec60e7441d32ff23ede27d8bca9cac82c
AlexisHeredia250518 <herediaalexisg@gmail.com>
2023-12-07 11:45:37 -0600

    Se agrego el proyecto

---
+++ ProductorConsumidor.Modelo/IBuffer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductorConsumidor.Modelo
+{
+     public interface IBuffer
+    {
+        int Buffer { get; set; }
+    }
+}
---
+++ ProductorConsumidor.SinSincronizacion/Program.cs
@@ -0,0 +1,13 @@
+﻿using ProductorConsumidor.Modelo;
+using ProductorConsumidor.SinSincronizacion;
+
+IBuffer recursoCompartido = new BufferImpl(); 
+Random generador = new Random(); 
+Productor productor = new Productor(recursoCompartido, generador); 
+Consumidor consumidor = new Consumidor(recursoCompartido, generador); 
+
+Thread hiloProductor = new Thread(new ThreadStart(productor.Producir)); 
+Thread hiloConsumidor = new Thread(new ThreadStart(consumidor.Consumir)); 
+hiloProductor.Name = "Productor"; 
+hiloConsumidor.Name = "Consumidor"; hiloProductor.Start(); 
+hiloConsumidor.Start();
